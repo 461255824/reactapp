30f380407fb37272521bd4928e65a9d69165164f
yangqiyan <qiyan.yang@cm-dt.com>
2018-07-09 18:23:10 +0800

    修改样式

--- src/comment/Login.js
+++ src/comment/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React,{ Component } from 'react';
 import {Button} from 'antd';
 class Hello extends Component {
   constructor(props){
@@ -12,14 +12,23 @@ class Hello extends Component {
   }
   render() {
     return (
-      <div className="hell">
+      <div style={container}>
         <Button type="primary" onClick={this.login}>
             点击登录
         </Button>
       </div>
       
     );
+
+
   }
+
+}
+var container = {
+    display:'flex',
+    justifyContent:"center",
+    alignItems:'center',
+    height:"100%"
 }
 
 export default Hello;
